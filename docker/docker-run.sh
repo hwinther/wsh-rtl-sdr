80da9abb0cb422398b19994cbd4aebbc07ea8f23
Hans Christian Winther-Sørensen <hc@wsh.no>
2026-01-14 12:20:30 +0100

    Update dockerfile base image to trixie and add publishing workflow

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v /dev/bus/usb:/dev/bus/usb --device-cgroup-rule='c 189:* rwm' -v /root/wsh-rtl-sdr/gsm_scan:/srv/gsm_scan -w /srv --cap-add=NET_ADMIN --cap-add=NET_RAW -it test
+sudo docker run --rm -v /dev/bus/usb:/dev/bus/usb --device-cgroup-rule='c 189:* rwm' -v $(pwd)/../gsm_scan:/srv/gsm_scan -w /srv --cap-add=NET_ADMIN --cap-add=NET_RAW -it ghcr.io/hwinther/wsh-rtl-sdr/gsm-tools:latest
